8578c629c862c01fba15af4de75bcb85b8a15590
Oswaldo Antonio Sanchez <mago.ads@gmail.com>
2020-02-10 07:37:20 -0600

    NEW CSS

--- js/scripts.js
+++ js/scripts.js
@@ -2955,3 +2955,19 @@
   }
 
 })(jQuery);
+
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 100) {
+      $('#global-nav').addClass('scrolled-nav');
+    } else if (scrollTop < 100) {
+      $('#global-nav').removeClass('scrolled-nav');
+    } 
+    
+  }); 
+  
+});
